369cef0b1fe15ee4cee1f55478e941a1622e924e
Dejan Budimir <dejbug@users.noreply.github.com>
2023-08-14 23:25:36 +0200

    fix sed test

--- src/tests.mk
+++ src/tests.mk
@@ -1,7 +1,7 @@
 # include library.mk
 
 SED_weforum='s/.*"id":"([0-9]+)","*__typename":"([^"]+)"(,"(name|title|url)":"([^"]+)")?(,"(title|url)":"([^"]+)")?.*/  ID | \1\nTYPE | \2\nARG1 | \5\nARG2 | \8\n/p'
-SED_linkedin='s/.*<div class="mb.*/START/p; s/.*<img.*data-delayed-url="([^"]+).*/IMG: \1/p; s|.*?<h2[^>]*>([^>]+)</h2>.*|TITLE: \1|p; s/.*?<p[^>]*>([^<]+).*/TEXT: \1/p'
+SED_linkedin='s/.*<div class="mb.*/START/; s/.*<img.*data-delayed-url="([^"]+).*/IMG: \1/; s|.*?<h2[^>]*>([^>]+)</h2>.*|TITLE: \1|; s/.*?<p[^>]*>([^<]+).*/TEXT: \1/'
 
 GawkFiles := $(wildcard *.gawk)
 
